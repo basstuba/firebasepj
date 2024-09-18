2e25c1e108aef69f47247e044f63ba9d3929e533
nutka <tuba4080@outlook.com>
2024-09-18 21:35:30 +0200

    confirm.vueの修正

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -2,6 +2,16 @@
     <div class="confirm">
         <h1>仮登録完了</h1>
         <p>仮登録が完了しました。メールをご確認ください。</p>
-        <button @click="this.$router.push('/')">戻る</button>
+        <button @click="backRoute">戻る</button>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        backRoute() {
+            this.$router.push('/');
+        }
+    },
+};
+</script>
